3ca34a3899b5a1ead55e63a8241e0ec8a372ab55
EC2 Default User <ec2-user@ip-172-31-15-121.ec2.internal>
2021-12-30 02:22:26 +0900

    finished

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
-class Book < ApplicationRecord 
+class Book < ApplicationRecord
 	belongs_to :user
 	validates :title, presence: true
 	validates :body ,presence: true, length: {maximum: 200}
@@ -8,5 +8,9 @@ class Book < ApplicationRecord
 
   def favorited_by?(user)
     Favorite.where(user_id: user.id, book_id: self.id).exists?
+  end
+
+  def self.term(time1, time2=Time.current)
+    self.where("? < created_at and created_at < ?", time1, time2)
   end
 end
